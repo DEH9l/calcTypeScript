a5a68f587e3743e5fb0399460cc98459bf5df81a
Денис <www.deh9l@gmail.com>
2022-01-31 12:34:48 +0200

    Done

---
+++ dist/typescript.js
@@ -0,0 +1,32 @@
+"use strict";
+var CalculatorHtml;
+(function (CalculatorHtml) {
+    CalculatorHtml.CALCULATE = document.getElementsByClassName('calc__getResult')[0];
+    CalculatorHtml.RESULT = document.getElementsByClassName('calc-result')[0];
+    CalculatorHtml.ClEARbTN = document.getElementsByClassName('calc__clearBtn')[0];
+    CalculatorHtml.CALCNUMBERS = document.getElementsByClassName('calc__numbers')[0];
+    CalculatorHtml.CALCOPERATORS = document.getElementsByClassName('calc__operators')[0];
+})(CalculatorHtml || (CalculatorHtml = {}));
+CalculatorHtml.ClEARbTN.addEventListener('click', () => CalculatorHtml.RESULT.innerText = null);
+CalculatorHtml.CALCNUMBERS.addEventListener('click', evt => {
+    if (evt.target.outerText !== '=') {
+        CalculatorHtml.RESULT.innerHTML += evt.target.outerText;
+    }
+});
+CalculatorHtml.CALCOPERATORS.addEventListener('click', evt => {
+    CalculatorHtml.RESULT.innerHTML += evt.target.outerText;
+});
+CalculatorHtml.CALCULATE.addEventListener('click', () => {
+    if (CalculatorHtml.RESULT.outerText.includes('+')) {
+        CalculatorHtml.RESULT.innerHTML = +CalculatorHtml.RESULT.outerText.split('+')[0] + +CalculatorHtml.RESULT.outerText.split('+')[1];
+    }
+    else if (CalculatorHtml.RESULT.outerText.includes('-')) {
+        CalculatorHtml.RESULT.innerHTML = +CalculatorHtml.RESULT.outerText.split('-')[0] - +CalculatorHtml.RESULT.outerText.split('-')[1];
+    }
+    else if (CalculatorHtml.RESULT.outerText.includes('/')) {
+        CalculatorHtml.RESULT.innerHTML = +CalculatorHtml.RESULT.outerText.split('/')[0] / +CalculatorHtml.RESULT.outerText.split('/')[1];
+    }
+    else if (CalculatorHtml.RESULT.outerText.includes('*')) {
+        CalculatorHtml.RESULT.innerHTML = +CalculatorHtml.RESULT.outerText.split('*')[0] * +CalculatorHtml.RESULT.outerText.split('*')[1];
+    }
+});
